60236faddda67968b32cfc9f98bdcf8313ef1610
snacksident <leak187@gmail.com>
2022-04-12 14:41:38 -0700

    parse through sample data for testing

--- parse.py
+++ parse.py
@@ -2,13 +2,15 @@ import json
 
 with open('apidata.txt') as f:
     the_api_data = f.read()
-print(f'the api data is {the_api_data}')
 #convert cached and stringified api data back to json for parsing
 json_object = json.loads(the_api_data)
+home_team = json_object["response"][0]["teams"]["home"]["name"]
+home_score = json_object["response"][0]["scores"]["home"]["points"]
+away_team = json_object["response"][0]["teams"]["visitors"]["name"]
+away_score = json_object["response"][0]["scores"]["visitors"]["points"]
 
-print(json_object)
-#output
-#<class 'dict'>
-
-#access first_name in dictionary
-# print(json_object["first_name"])
+print(f'the visiting team for sample data is: {away_team}')
+print(f'the home team for sample data is: {home_team}')
+print(f'the visitors score for sample data is: {away_score}')
+print(f'the home score for sample data is {home_score}')
+print(f'the final score is: {home_team}:{home_score}, {away_team}:{away_score}')
