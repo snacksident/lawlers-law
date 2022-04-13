88029ca0fc31a3adf4391279614f6ffb760a65bd
snacksident <leak187@gmail.com>
2022-04-13 12:21:42 -0700

    pseudocode for flow of checking API during games (not unlimited API calls. attempting conservative use of limited calls)

--- sportradar.py
+++ sportradar.py
@@ -5,6 +5,9 @@ import json
 
 load_dotenv()
 def check_todays_games():
+    '''
+    checks sportradar api for any games happening today.  for now, adds them to text file for further viewing
+    '''
     API_KEY = os.getenv('SPORTRADAR_API_KEY')
     #gonna need to change dates in URL with a var
     url = f'http://api.sportradar.us/nba/trial/v7/en/games/2022/04/12/schedule.json?api_key={API_KEY}'
@@ -16,10 +19,6 @@ def check_todays_games():
     todays_games = open("todaysgames.txt","w")
     todays_json = json.dumps(todays_schedule.json())
     todays_games.write(todays_json)
-
-# check if there's any games happening today
-    # if there are games, grab and save game ID's to use in the game url
-    # if no games, do not make any more API calls until next day
 
 
 # hit game url endpoint (60 mins?) after game start to check scores.
@@ -46,12 +45,14 @@ away_team = {
     "score": 0,
     "name": None,
 }
-    # if a team is "close to" 100, do another ping shortly after 
-    
-    # if a team is AT OR PAST 100, we can stop pinging until the game is over. save teams ID
-    # send tweet (f'we have a winner!  {team who hit 100} bested {other team} with {time left in the game} to spare!!!')
-    # at game end, check winning team ID vs first_team_to100 ID
-
-test_file = open("sportradar.txt","w")
-json_dumps = json.dumps(current_game.json())
-test_file.write(json_dumps)
+# while (either teams score) < 100:
+    #if (either teams score) > 90:
+        #do a call in 3 mins
+    #elif (either teams score) > 80:
+        #do a call in 7 mins
+    #else:
+        #call in 10 mins
+# if (either team score) > 100:
+    #tweet out that we have a team over 100 (a 'winner' in lawlers eyes)
+    #save gameID
+    #follow up when game ends to determine if law was t/f
--- time.py
+++ time.py
@@ -24,3 +24,15 @@ while True:
 # while True:
 #     foo()
 #     time.sleep(1)
+
+# while (either teams score) < 100:
+    #if (either teams score) > 90:
+        #do a call in 3 mins
+    #elif (either teams score) > 80:
+        #do a call in 7 mins
+    #else:
+        #call in 10 mins
+# if (either team score) > 100:
+    #tweet out that we have a team over 100 (a 'winner' in lawlers eyes)
+    #save gameID
+    #follow up when game ends to determine if law was t/f
