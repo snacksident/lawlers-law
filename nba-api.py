66a29eaa16aff6cb750fb3831f352bf09fdd1894
snacksident <leak187@gmail.com>
2022-04-14 16:50:08 -0700

    delete old test files, work on nba-api.py to replace sportradar.py (ease of use, longevity)

--- nba-api.py
+++ nba-api.py
@@ -22,4 +22,26 @@ def get_todays_games():
     todays_json = json.dumps(todays_schedule.json())
     todays_games.write(todays_json)
 
-get_todays_games()
+def get_score_by_game_id(game_id):
+    API_KEY = os.getenv('RAPID_API_KEY')
+    url = "https://api-nba-v1.p.rapidapi.com/games"
+    querystring = {"id": game_id}
+    headers = {
+	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+	"X-RapidAPI-Key": API_KEY
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    current_score_info = open("nba_api.txt","w")
+    current_score_json = json.dumps(response.json())
+    current_score_info.write(current_score_json)
+
+with open("rapid_api_todaysgames.txt") as todays_games:
+    todays_docket = todays_games.read()
+json_docket = json.loads(todays_docket)
+
+def assign_game_data():
+    #check if any games on the menu today
+    #if no games, set 24h timer to check for games again?
+    #if games today
+        #log games info (id,start_time,home{name, score},away{home,score})
+    pass
--- tweet.py
+++ tweet.py
@@ -10,11 +10,10 @@ client = tweepy.Client(consumer_key=os.getenv('CONSUMER_KEY'),
                        access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'))
 # create tweet with token credentials
 text_to_tweet = ""
-response = client.create_tweet(text='hello again')
+# response = client.create_tweet(text='testing testing 1,2,3 its me ralph lawler ')
 def send_tweet(tweet_text):
     '''
     creates a plaintext tweet from @lawlers_laws account.  content of tweet is passed in as "tweet_text" var
     '''
     tweet_response = client.create_tweet(text=tweet_text)
 # print response from twitter to console, verifying that the tweet was sent
-print(response)
